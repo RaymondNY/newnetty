d163f43252ac362714104b91dbcc9ad722cc2e02
zqx <317171584@qq.com>
2019-12-05 15:39:49 +0800

    aa

---
+++ src/test/java/com/example/newnetty/NewnettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newnetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewnettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
