a80b1f87a9f9421764040c516c878832c5be5dbc
zhangqixuan <18640481170@163.com>
2020-05-18 16:19:51 +0800

    pom

--- src/main/java/com/example/newnetty/ch2/FarmConstant.java
+++
@@ -1,8 +0,0 @@
-package com.example.newnetty.ch2;
-
-import io.netty.util.AttributeKey;
-
-public class FarmConstant {
-    private FarmConstant(){}
-    public static final AttributeKey<String> CHANNEL_MAC_ADDRESS = AttributeKey.valueOf("channel.mac.address");
-}
--- src/main/java/com/example/newnetty/router/App.java
+++
@@ -1,8 +0,0 @@
-package com.example.newnetty.router;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        HttpServer httpServer = new HttpServer(8899);
-        httpServer.start();
-    }
-}
